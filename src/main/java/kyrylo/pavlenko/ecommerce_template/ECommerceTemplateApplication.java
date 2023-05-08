4c6eac3b8064c8c13f65067c8667a1a5af107977
Kyrylo Pavlenko <kyrylo.pavlenko.w@gmail.com>
2023-05-08 11:40:49 +0300

    Initial commit

---
+++ src/main/java/kyrylo/pavlenko/ecommerce_template/ECommerceTemplateApplication.java
@@ -0,0 +1,13 @@
+package kyrylo.pavlenko.ecommerce_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kyrylo/pavlenko/ecommerce_template/ECommerceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package kyrylo.pavlenko.ecommerce_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
